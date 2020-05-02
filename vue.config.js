64cc2fa8e977c0f2d300d703f55a13ba1e444a01
jake <jake@jakecoffman.com>
2020-05-02 09:10:36 -0500

    trying to fix new content not downloading

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '/ws': {
-                // target: 'http://localhost:8112',
-                target: 'https://resistance.jakecoffman.com',
-                changeOrigin: true,
-                ws: true
-            }
-        }
+  devServer: {
+    proxy: {
+      '/ws': {
+        // target: 'http://localhost:8112',
+        target: 'https://resistance.jakecoffman.com',
+        changeOrigin: true,
+        ws: true
+      }
     }
+  },
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    }
+  },
 }
